9a77bab0057cd7808a79bb4976acc303a24befaf
Matěj Jaroš <matysekjaros@gmail.com>
2021-06-05 13:16:32 +0200

    aktual

--- geografie/js/geografie.js
+++ geografie/js/geografie.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
     $(".land").on("mouseover", function(){
-        $(this).css("fill", "red");  
+        if($("#kraje:checked").val()) {
+            $(this).css("fill", "red"); 
+        }
+         
     });
     $(".land").on("mouseout", function(){
         $(this).css("fill", "lightgray");
     });
     $(".city").on("click", function(){
-        $(".city").css("fill", "gray");
-        $(this).css("fill", "green");  
+        if($("#mesta:checked").val()) {
+            $(".city").css("fill", "gray");
+            $(this).css("fill", "green"); 
+        }  
     });
     $(".vypinac").on("click", function(){
         //$(".city").css("fill", "green");
--- historie/historie.js
+++ historie/historie.js
@@ -1,89 +1,19 @@
 const act = "active1";
+const udalostiText = ".udalostiText"
 $(document).ready(function(){
     $(".udalostiText").hide();
-
-
     $(".udalosti tr").on("mouseover", function(){
-        $(".udalostiText").slideUp();
-        $(".tr udalostiText").slideDown(1000);
+        //$(".udalostiText").slideUp();
+        $(".udalostiText").slideDown(1000);
         $(this).addClass("bg-secondary text-white");
-        $(".udalosti td").removeClass("bg-secondary text-white");
     });
 
-    $("#udalosti22").mouseover(function(){
-        $("#udalostiText1").slideUp();
-        $("#udalostiText2").slideDown(1000);
-        $("#udalostiText3").slideUp();
-        $("#udalostiText4").slideUp();
-        $("#udalostiText5").slideUp();
-        $("#udalostiText6").slideUp();
-        $("#udalosti1").removeClass("bg-secondary text-white");
-        $("#udalosti2").addClass("bg-secondary text-white");
-        $("#udalosti3").removeClass("bg-secondary text-white");
-        $("#udalosti4").removeClass("bg-secondary text-white");
-        $("#udalosti5").removeClass("bg-secondary text-white");
-        $("#udalosti6").removeClass("bg-secondary text-white");
+    $(".udalosti tr").on("mouseout", function(){
+        $(".udalostiText").slideUp();
+        //$(".udalostiText").slideDown(1000);
+        $(this).removeClass("bg-secondary text-white");
     });
 
-    $("#udalosti33").mouseover(function(){
-        $("#udalostiText1").slideUp();
-        $("#udalostiText2").slideUp();
-        $("#udalostiText3").slideDown(1000);
-        $("#udalostiText4").slideUp();
-        $("#udalostiText5").slideUp();
-        $("#udalostiText6").slideUp();
-        $("#udalosti1").removeClass("bg-secondary text-white");
-        $("#udalosti2").removeClass("bg-secondary text-white");
-        $("#udalosti3").addClass("bg-secondary text-white");
-        $("#udalosti4").removeClass("bg-secondary text-white");
-        $("#udalosti5").removeClass("bg-secondary text-white");
-        $("#udalosti6").removeClass("bg-secondary text-white");
-    });
-
-    $("#udalosti44").mouseover(function(){
-        $("#udalostiText1").slideUp();
-        $("#udalostiText2").slideUp();
-        $("#udalostiText3").slideUp();
-        $("#udalostiText4").slideDown(1000);
-        $("#udalostiText5").slideUp();
-        $("#udalostiText6").slideUp();
-        $("#udalosti1").removeClass("bg-secondary text-white");
-        $("#udalosti2").removeClass("bg-secondary text-white");
-        $("#udalosti3").removeClass("bg-secondary text-white");
-        $("#udalosti4").addClass("bg-secondary text-white");
-        $("#udalosti5").removeClass("bg-secondary text-white");
-        $("#udalosti6").removeClass("bg-secondary text-white");
-    });
-
-    $("#udalosti55").mouseover(function(){
-        $("#udalostiText1").slideUp();
-        $("#udalostiText2").slideUp();
-        $("#udalostiText3").slideUp();
-        $("#udalostiText4").slideUp();
-        $("#udalostiText5").slideDown(1000);
-        $("#udalostiText6").slideUp();
-        $("#udalosti1").removeClass("bg-secondary text-white");
-        $("#udalosti2").removeClass("bg-secondary text-white");
-        $("#udalosti3").removeClass("bg-secondary text-white");
-        $("#udalosti4").removeClass("bg-secondary text-white");
-        $("#udalosti5").addClass("bg-secondary text-white");
-        $("#udalosti6").removeClass("bg-secondary text-white");
-    });
-
-    $("#udalosti66").mouseover(function(){
-        $("#udalostiText1").slideUp();
-        $("#udalostiText2").slideUp();
-        $("#udalostiText3").slideUp();
-        $("#udalostiText4").slideUp();
-        $("#udalostiText5").slideUp();
-        $("#udalostiText6").slideDown(1000);
-        $("#udalosti1").removeClass("bg-secondary text-white");
-        $("#udalosti2").removeClass("bg-secondary text-white");
-        $("#udalosti3").removeClass("bg-secondary text-white");
-        $("#udalosti4").removeClass("bg-secondary text-white");
-        $("#udalosti5").removeClass("bg-secondary text-white");
-        $("#udalosti6").addClass("bg-secondary text-white");
-    });
     $("#pT1").hide();
     $(".jmena tr").on("click", function(){
         //$("#pT1").fadeIn(1000);
