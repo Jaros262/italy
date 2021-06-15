d3cfb4fd232a4fae861a2d4977f0def9214a5d46
Matěj Jaroš <matysekjaros@gmail.com>
2021-06-15 22:07:14 +0200

    umřu

--- historie/historie.js
+++ historie/historie.js
@@ -1,20 +1,69 @@
+const events = [{
+    "year":"995",
+    "event":"▼ Založení říma",
+    "detail":" Podle pověsti byl Řím založen 21. dubna 753 př. n. l. v ústí řeky Tibery v oblasti zvané Latium [lácium] záhadnými Etrusky. Tuto pověst uvádí jako první římský historik Varro, který žil v 1. století př. n. l.Aeneas, syn dardanského krále Anchíse a bohyně lásky Venuše, patřil k nejstatečnějším obráncům Tróje. Unikl z hořícího města spolu se svým otcem Anchisem a synem Askaniem. Podle Diova příkazu si měl nový domov hledat v Itálii. Po sedmileté dobrodružné plavbě vplul do ústí Tibery a založil tam město Lavinium, které nazval podle své manželky Lavinie, dcery místního krále Latina. Aeneův syn Ascanius, zvaný též Iulus (od něj odvozoval svůj původ rod Iuliů, z nějž pocházel Caesar), založil pak město nové, Alba Longu, přímého předchůdce Říma. Dvanáctým nástupcem Askania byl král Prokas, který měl dva syny, staršího Numitora a mladšího Amulia. Amulius po smrti svého otce sesadil z trůnu svého staršího bratra Numitora, dal popravit jeho syny a dceru Rheu Silvii přinutil stát se kněžkou bohyně Vesty. Rhea Silvia sice jako kněžka slíbila věčné panenství, podlehla však bohu války Martovi a porodila mu dvojčata Romula a Rema.",
+    "url":"https://cs.wikipedia.org/wiki/Vyvra%C5%BEd%C4%9Bn%C3%AD_Slavn%C3%ADkovc%C5%AF"
+},
+{
+    "year":"996",
+    "event":"▼ Druha udalost",
+    "detail":"Unikl z hořícího města spolu se svým otcem Anchisem a synem Askaniem. Podle Diova příkazu si měl nový domov hledat v Itálii. Po sedmileté dobrodružné plavbě vplul do ústí Tibery a založil tam město Lavinium, které nazval podle své manželky Lavinie, dcery místního krále Latina. Aeneův syn",
+    "url":"https://www.google.com/search?q=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&rlz=1C1AVFC_enCZ941CZ941&oq=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&aqs=chrome..69i57.10119j0j7&sourceid=chrome&ie=UTF-8"
+},
+{
+    "year":"997",
+    "event":"▼ Druha udalost",
+    "detail":"Unikl z hořícího města spolu se svým otcem Anchisem a synem Askaniem. Podle Diova příkazu si měl nový domov hledat v Itálii. Po sedmileté dobrodružné plavbě vplul do ústí Tibery a založil tam město Lavinium, které nazval podle své manželky Lavinie, dcery místního krále Latina. Aeneův syn",
+    "url":"https://www.google.com/search?q=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&rlz=1C1AVFC_enCZ941CZ941&oq=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&aqs=chrome..69i57.10119j0j7&sourceid=chrome&ie=UTF-8"
+},
+{
+    "year":"998",
+    "event":"▼ Druha udalost",
+    "detail":"Unikl z hořícího města spolu se svým otcem Anchisem a synem Askaniem. Podle Diova příkazu si měl nový domov hledat v Itálii. Po sedmileté dobrodružné plavbě vplul do ústí Tibery a založil tam město Lavinium, které nazval podle své manželky Lavinie, dcery místního krále Latina. Aeneův syn",
+    "url":"https://www.google.com/search?q=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&rlz=1C1AVFC_enCZ941CZ941&oq=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&aqs=chrome..69i57.10119j0j7&sourceid=chrome&ie=UTF-8"
+},
+{
+    "year":"999",
+    "event":"▼ Druha udalost",
+    "detail":"Unikl z hořícího města spolu se svým otcem Anchisem a synem Askaniem. Podle Diova příkazu si měl nový domov hledat v Itálii. Po sedmileté dobrodružné plavbě vplul do ústí Tibery a založil tam město Lavinium, které nazval podle své manželky Lavinie, dcery místního krále Latina. Aeneův syn",
+    "url":"https://www.google.com/search?q=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&rlz=1C1AVFC_enCZ941CZ941&oq=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&aqs=chrome..69i57.10119j0j7&sourceid=chrome&ie=UTF-8"
+}
+];
+const persons = [{
+    "date":"995",
+    "name":"▼ Založení říma",
+    "detail":" Podle pověsti byl Řím založen 21. dubna 753 př. n. l. v ústí řeky Tibery v oblasti zvané Latium [lácium] záhadnými Etrusky. Tuto pověst uvádí jako první římský historik Varro, který žil v 1. století př. n. l.Aeneas, syn dardanského krále Anchíse a bohyně lásky Venuše, patřil k nejstatečnějším obráncům Tróje. Unikl z hořícího města spolu se svým otcem Anchisem a synem Askaniem. Podle Diova příkazu si měl nový domov hledat v Itálii. Po sedmileté dobrodružné plavbě vplul do ústí Tibery a založil tam město Lavinium, které nazval podle své manželky Lavinie, dcery místního krále Latina. Aeneův syn Ascanius, zvaný též Iulus (od něj odvozoval svůj původ rod Iuliů, z nějž pocházel Caesar), založil pak město nové, Alba Longu, přímého předchůdce Říma. Dvanáctým nástupcem Askania byl král Prokas, který měl dva syny, staršího Numitora a mladšího Amulia. Amulius po smrti svého otce sesadil z trůnu svého staršího bratra Numitora, dal popravit jeho syny a dceru Rheu Silvii přinutil stát se kněžkou bohyně Vesty. Rhea Silvia sice jako kněžka slíbila věčné panenství, podlehla však bohu války Martovi a porodila mu dvojčata Romula a Rema.",
+    "url":"https://cs.wikipedia.org/wiki/Vyvra%C5%BEd%C4%9Bn%C3%AD_Slavn%C3%ADkovc%C5%AF"
+},
+{
+    "date":"996",
+    "name":"▼ Založení říma",
+    "detail":"Unikl z hořícího města spolu se svým otcem Anchisem a synem Askaniem. Podle Diova příkazu si měl nový domov hledat v Itálii. Po sedmileté dobrodružné plavbě vplul do ústí Tibery a založil tam město Lavinium, které nazval podle své manželky Lavinie, dcery místního krále Latina. Aeneův syn",
+    "url":"https://www.google.com/search?q=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&rlz=1C1AVFC_enCZ941CZ941&oq=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&aqs=chrome..69i57.10119j0j7&sourceid=chrome&ie=UTF-8"
+},
+{
+    "date":"997",
+    "name":"▼ Založení říma",
+    "detail":"Unikl z hořícího města spolu se svým otcem Anchisem a synem Askaniem. Podle Diova příkazu si měl nový domov hledat v Itálii. Po sedmileté dobrodružné plavbě vplul do ústí Tibery a založil tam město Lavinium, které nazval podle své manželky Lavinie, dcery místního krále Latina. Aeneův syn",
+    "url":"https://www.google.com/search?q=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&rlz=1C1AVFC_enCZ941CZ941&oq=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&aqs=chrome..69i57.10119j0j7&sourceid=chrome&ie=UTF-8"
+},
+{
+    "date":"998",
+    "name":"▼ Založení říma",
+    "detail":"Unikl z hořícího města spolu se svým otcem Anchisem a synem Askaniem. Podle Diova příkazu si měl nový domov hledat v Itálii. Po sedmileté dobrodružné plavbě vplul do ústí Tibery a založil tam město Lavinium, které nazval podle své manželky Lavinie, dcery místního krále Latina. Aeneův syn",
+    "url":"https://www.google.com/search?q=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&rlz=1C1AVFC_enCZ941CZ941&oq=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&aqs=chrome..69i57.10119j0j7&sourceid=chrome&ie=UTF-8"
+},
+{
+    "date":"999",
+    "name":"▼ Založení říma",
+    "detail":"Unikl z hořícího města spolu se svým otcem Anchisem a synem Askaniem. Podle Diova příkazu si měl nový domov hledat v Itálii. Po sedmileté dobrodružné plavbě vplul do ústí Tibery a založil tam město Lavinium, které nazval podle své manželky Lavinie, dcery místního krále Latina. Aeneův syn",
+    "url":"https://www.google.com/search?q=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&rlz=1C1AVFC_enCZ941CZ941&oq=Unikl+z+ho%C5%99%C3%ADc%C3%ADho+m%C4%9Bsta+spolu+se+sv%C3%BDm+otcem+Anchisem+a+synem+Askaniem.+Podle+Diova+p%C5%99%C3%ADkazu+si+m%C4%9Bl+nov%C3%BD+domov+hledat+v+It%C3%A1lii.+Po+sedmilet%C3%A9+dobrodru%C5%BEn%C3%A9+plavb%C4%9B+vplul+do+%C3%BAst%C3%AD+Tibery+a+zalo%C5%BEil+tam+m%C4%9Bsto+Lavinium%2C+kter%C3%A9+nazval+podle+sv%C3%A9+man%C5%BEelky+Lavinie%2C+dcery+m%C3%ADstn%C3%ADho+kr%C3%A1le+Latina.+Aene%C5%AFv+syn&aqs=chrome..69i57.10119j0j7&sourceid=chrome&ie=UTF-8"
+}
+];
 const act = "active1";
 const udalostiText = ".udalostiText"
 $(document).ready(function(){
-    $(".udalostiText").hide();
-    $(".udalosti tr").on("mouseover", function(){
-        //$(".udalostiText").slideUp();
-        $(".udalostiText").slideDown(1000);
-        $(this).addClass("bg-secondary text-white");
-    });
-
-    $(".udalosti tr").on("mouseout", function(){
-        $(".udalostiText").slideUp();
-        //$(".udalostiText").slideDown(1000);
-        $(this).removeClass("bg-secondary text-white");
-    });
-
-    $("#pT1").hide();
+    //$("#pT1").hide();
     $(".jmena tr").on("click", function(){
         //$("#pT1").fadeIn(1000);
         $(".jmena tr").removeClass(act);
@@ -30,4 +79,97 @@ $(document).ready(function(){
         $(this).css("color", "black");
         console.log(this);
     });
-  });
+});
+
+  events.forEach((event)=>{
+    $(".udalosti tbody").append(`<tr>
+        <td class="event-year">${event.year}</td>
+        <td>
+          <p class="event-name"><i class="fas"></i> <a href="${event.url}" target="_new" > ${event.event}</a></p>
+          <p class="event-detail">${event.detail}</p>
+        </td>            
+        <td></td>
+    </tr>`);
+});
+
+/* Po načtení stránky se skryjí všechny detaily událostí */
+$(".event-detail").hide();
+
+/* Při najetí kurzoru myši na ikonu nebo odkaz dojde k následujícím akcím: */
+$(".event-name i, .event-name a").on("mouseover", function(){
+    /* Ze všech řádků tabulky se odstraní dvě uvedené třídy */
+    $(".udalosti tbody tr").removeClass("bg-secondary text-white");
+    /* Tyto dvě třídy sepřidají jen rodičovskému řádku (.parents("tr")) toho (this) prvku, na který zrovna ukázala myš */
+    $(this).parents("tr").addClass("bg-secondary text-white");
+    /* Nejprve zajistíme skrytí všech detailů událostí */
+    $(".event-detail").hide();
+    /* Poté ukážeme pouze ten detail, který následuje po prvku, na který zrovna ukázala myš */
+    /* Zde je použito tzv. traverzování - metodou parent() nejprve "traverzujeme" na rodiče aktivního prvku (odstavec), 
+       poté metodou next() vybereme nejbližšího následujícího sourozence (odstavec s detailem) */
+    /* Zároveň zde, ale i na jiných místech, využíváme tzv. řetězení (chaining), kdy můžeme volat několik metod v řadě */   
+    $(this).parent().next().show(500);
+}); 
+
+persons.forEach((event)=>{
+    $("#personsInfo").append(`
+        <div class="col-4">
+            <table class="table jmena">
+              <tbody>
+                <tr>
+                  <td>Julius Cesar</td>
+                </tr>
+                <tr>
+                  <td>Aj ty Brute</td>
+                </tr>
+                <tr>
+                  <td>Mussolini</td>
+                </tr>
+                <tr>
+                  <td>Král Franz</td>
+                </tr>
+                <tr>
+                  <td>Ice Cube</td>
+                </tr>
+              </tbody>
+            </table>
+        </div>
+    <div class="col-8">
+    <table class="table table-inverse table-bordered" id="personsInfoFunction">
+        <thead class="thead-inverse">
+            <tr>
+                <th class="persons-date">${persons.date}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <h4 class="persons-name">${persons.name}</h4>
+                    <p class="persons-detail">${persons.detail}</p>
+                </td>
+            </tr>
+            <tr>
+                <td>Odkaz:<a href="${persons.url}" target="_new" > ${persons.url}</a> hustý odkaz</a></td>
+            </tr>
+        </tbody>
+    </table>          
+    </div>
+    `);
+});
+
+/* Po načtení stránky se skryjí všechny detaily událostí */
+$("#personsInfoFunction").hide();
+
+/* Při najetí kurzoru myši na ikonu nebo odkaz dojde k následujícím akcím: */
+$("#personsInfo tr").on("mouseover", function(){
+    /* Ze všech řádků tabulky se odstraní dvě uvedené třídy */
+    $(".udalosti tbody tr").removeClass("bg-secondary text-white");
+    /* Tyto dvě třídy sepřidají jen rodičovskému řádku (.parents("tr")) toho (this) prvku, na který zrovna ukázala myš */
+    $(this).parents("tr").addClass("bg-secondary text-white");
+    /* Nejprve zajistíme skrytí všech detailů událostí */
+    $("#personsInfoFunction").hide();
+    /* Poté ukážeme pouze ten detail, který následuje po prvku, na který zrovna ukázala myš */
+    /* Zde je použito tzv. traverzování - metodou parent() nejprve "traverzujeme" na rodiče aktivního prvku (odstavec), 
+       poté metodou next() vybereme nejbližšího následujícího sourozence (odstavec s detailem) */
+    /* Zároveň zde, ale i na jiných místech, využíváme tzv. řetězení (chaining), kdy můžeme volat několik metod v řadě */   
+    $(this).parent().next().show(500);
+});
