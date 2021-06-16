097094785d62da55bc3a7313042971f5471c642b
student <matysekjaros@gmail.com>
2021-06-16 12:16:01 +0200

    server base

--- public/historie/historie.js
+++ public/historie/historie.js
@@ -33,31 +33,36 @@ const persons = [{
     "date":"13. července 100 př. n. l. – 15. března 44 př. n. l. Řím",
     "id":"Julius Ceasar",
     "detail":"Gaius Julius Caesar byl vojevůdce a politik a jeden z nejmocnějších mužů antické historie. Sehrál klíčovou roli v procesu zániku římské republiky a její transformace v císařství. Z jeho jména pochází titul caesar a odvozené císař (rusky car, německy Kaiser, dánsky kejser, polsky cesarz aj.), kterým ale sám Caesar nebyl; prvním římským císařem se stal až jeho prasynovec a adoptivní syn Augustus (vládnoucí v letech 27 př. n. l. – 14 n. l.). (Starořímská výslovnost byla [kaisar]; dnešní výslovnost [cézar] pochází až z pozdní antiky či raného středověku.)",
-    "url":"https://cs.wikipedia.org/wiki/Julius_Caesar"
+    "url":"https://cs.wikipedia.org/wiki/Julius_Caesar",
+    "pic":"caesar.jpg"
 },
 {
     "date":"31. října 1451, Janov – 20. května 1506, Valladolid",
     "id":"Kryštof Kolumbus",
     "detail":"Kryštof Kolumbus (italsky Cristoforo Colombo) byl janovský mořeplavec a kolonizátor narozený v Janovské republice v dnešní severozápadní Itálii. Ve službách Katolických králů Španělska uskutečnil čtyři plavby přes Atlantský oceán, které v Evropě probudily zájem o nový kontinent, později nazvaný Amerika. Tyto výpravy a Kolumbovy snahy o založení trvalých sídel na Hispaniole zahájily španělskou kolonizaci, která vyústila v soutěž evropských mocností o kolonizaci Jižní i Severní Ameriky.",
-    "url":"https://cs.wikipedia.org/wiki/Kry%C5%A1tof_Kolumbus"
+    "url":"https://cs.wikipedia.org/wiki/Kry%C5%A1tof_Kolumbus",
+    "pic":"columbus.jpg"
 },
 {
     "date":"15. dubna 1452 – 2. května 1519 Le Clos Lucé u Amboise, Francie",
     "id":"Leonardo da Vinci",
     "detail":"Leonardo da Vinci, rodným jménem Leonardo di ser Piero byl italský malíř, čelný představitel renesanční malby, mimo jiné autor nejslavnějšího obrazu všech dob, portrétu zvaného Mona Lisa (kolem 1503–07, Louvre, Paříž). Proslul také jako všestranná renesanční osobnost: vedle malířství byl i sochař, architekt, přírodovědec, hudebník, spisovatel, vynálezce a konstruktér. Nemalou měrou se podílel také na zdůrazňování práv zvířat, s čímž pravděpodobně souvisel i jeho možný vegetariánský jídelníček.",
-    "url":"https://cs.wikipedia.org/wiki/Leonardo_da_Vinci"
+    "url":"https://cs.wikipedia.org/wiki/Leonardo_da_Vinci",
+    "pic":"davinci.jpg"
 },
 {
     "date":"15. února 1564, Pisa – 8. ledna 1642, Arcetri",
     "id":"Galileo Galilei",
     "detail":"Galileo Galilei byl toskánský astronom, filosof a fyzik těsně spjatý s vědeckou revolucí. Mezi jeho úspěchy řadíme vylepšení dalekohledu, rozmanitá astronomická pozorování, první z Newtonových zákonů pohybu a účinnou podporu Koperníka. Často je uváděn jako „otec moderní astronomie“, „otec moderní fyziky“ a dokonce „otec vědy“. Jeho experimentální činnost je obecně považována za důležitý doplněk spisů Francise Bacona, jimiž byla založena moderní vědecká metoda. Galileova kariéra se kryla s tvůrčím obdobím Johannese Keplera. Galileovo dílo je považováno za nejvýznamnější průlom od dob Aristotelových, ač on sám nikdy heliocentrický systém neprokázal. Toto byl také důvod sporu s papežem Urbanem VIII., který jej žádal, aby věc prezentoval pouze jako hypotézu, než budou objeveny nepopíratelné důkazy. Galileo je považován za průkopníka svobodného vědeckého zkoumání. Za nedovolené publikování svých vědeckých názorů byl dvakrát postaven před inkviziční soud a nakonec i odsouzen. Trest žaláře mu byl zmírněn na domácí vězení, ve kterém byl držen až do své smrti.",
-    "url":"https://cs.wikipedia.org/wiki/Galileo_Galilei"
+    "url":"https://cs.wikipedia.org/wiki/Galileo_Galilei",
+    "pic":"galileo.jpeg"
 },
 {
     "date":"29. července 1883 Dovia – 28. dubna 1945 Giulino",
     "id":"Benito Mussolini",
     "detail":"Benito Amilcare Andrea Mussolini byl italský premiér, politik a diktátor, spolutvůrce a zakladatel fašismu. Jeho otec Alessandro Mussolini byl nejdříve kovářem v Predappiu a pak majitelem hostince ve Forli, matka byla učitelkou. Rodina byla revolucionářská (Benito byl pojmenován po Benitu Juárezovi). Vystudoval učitelský ústav (1901), poté jeden rok působil jako učitel. Roku 1902 uprchl do Švýcarska, aby se vyhnul vojenské službě. Zde se živil různými nekvalifikovanými pracemi, kupř. pomocník zedníka, téhož roku se stal členem Italské socialistické strany. Podle některých zdrojů se ve Švýcarsku stýkal s ruskými emigranty a s bolševiky, např. i V. I. Leninem. Když už ve Švýcarsku nenašel žádné pracovní místo, musel žebrat, byl zatčen za potulku a nakonec vykázán ze země.",
-    "url":"https://cs.wikipedia.org/wiki/Benito_Mussolini"
+    "url":"https://cs.wikipedia.org/wiki/Benito_Mussolini",
+    "pic":"mussolini.jpg"
 }
 ];
 const act = "active1";
@@ -87,8 +92,11 @@ $(document).ready(function(){
             <tr>
                 <td>Odkaz:<a href="${person.url}" target="_new" > ${person.url}</a></td>
             </tr>
+                 
         </tbody>
-    </table>          
+        
+    </table>
+    <img src="img/${person.pic}" style="width: 30%">
         `);
     });
     $(".jmena tr").on("mouseover", function(){
@@ -119,52 +127,6 @@ $(".event-name i, .event-name a").on("mouseover", function(){
     $(".event-detail").hide();
     $(this).parent().next().show(500);
 }); 
-
-/*persons.forEach((persons)=>{
-    $("#personsInfo").append(`
-        <div class="col-4">
-            <table class="table jmena">
-              <tbody>
-                <tr>
-                  <td>Julius Cesar</td>
-                </tr>
-                <tr>
-                  <td>Aj ty Brute</td>
-                </tr>
-                <tr>
-                  <td>Mussolini</td>
-                </tr>
-                <tr>
-                  <td>Král Franz</td>
-                </tr>
-                <tr>
-                  <td>Ice Cube</td>
-                </tr>
-              </tbody>
-            </table>
-        </div>
-    <div class="col-8">
-    <table class="table table-inverse table-bordered" id="personsInfoFunction">
-        <thead class="thead-inverse">
-            <tr>
-                <th class="persons-date">${persons.date}</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>
-                    <h4 class="persons-name">${persons.name}</h4>
-                    <p class="persons-detail">${persons.detail}</p>
-                </td>
-            </tr>
-            <tr>
-                <td>Odkaz:<a href="${persons.url}" target="_new" > ${persons.url}</a> hustý odkaz</a></td>
-            </tr>
-        </tbody>
-    </table>          
-    </div>
-    `);
-});*/
 
 $("#personsInfoFunction").hide();
 
---
+++ server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/events', (req, res) => {
+    readJSON('data/karta.json')
+    .then(data => res.send(data))
+    .catch(err => res.send('Chyba lávky', err));
+});
+
+app.get('/api/events/:index', (req, res) => {
+  readJSON('data/karta.json')
+  .then(data => res.send(data[req.params.index]))
+  .catch(err => res.send('Chyba lávky', err));
+});
+
+const PORT = process.env.PORT || 6060;
+server.listen(PORT,() => console.log(`Server běží na portu ${PORT}`));
